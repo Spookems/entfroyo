3c33817a6b39cb51722b22cbe5f8fa591af2708e
Marshall Brandon FESSEY <s6115598@linux19.scm.tees.ac.uk>
2018-02-27 12:15:23 +0000

    Initial commit

---
+++ app/src/main/java/uk/ac/tees/com2060/froyo/ent_froyo/MainActivity.kt
@@ -0,0 +1,12 @@
+package uk.ac.tees.com2060.froyo.ent_froyo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
